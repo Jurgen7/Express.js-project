d93f2fdc3c6fcdbf0a3e730058e61556b95f3633
Jurgen7 <jurgenskenderi7@gmail.com>
2024-03-19 01:24:28 +0100

    feat: added jwt token and validation

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,16 +3,16 @@ const Contact = require("../models/contactModel")
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find()
+    const contacts = await Contact.find({ user_id: req.user.id })
     res.status(200).json(contacts)
 })
 
 //@desc Create new contacts
 //@route POST /api/contacts
-//@access public
+//@access private
 
 const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is :", req.body)
@@ -25,6 +25,7 @@ const createContact = asyncHandler(async (req, res) => {
         name,
         email,
         phone,
+        user_id: req.user.id,
     })
 
     res.status(201).json(contact)
@@ -52,6 +53,11 @@ const updateContact = asyncHandler(async (req, res) => {
         throw new Error("Contact not found")
     }
 
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403)
+        throw new Error("User don't have permission to update other user contacts")
+    }
+
     const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true })
 
     res.status(200).json(updatedContact)
@@ -66,7 +72,13 @@ const deleteContact = asyncHandler(async (req, res) => {
         res.status(404)
         throw new Error("Contact not found")
     }
-    await Contact.remove()
+
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403)
+        throw new Error("User don't have permission to delete other user contacts")
+    }
+
+    await Contact.deleteOne({ _id: req.params.id })
     res.status(200).json(contact)
 })
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 //@desc Register a user
 //@route POST /api/users/register
@@ -48,6 +49,26 @@ const loginUser = asyncHandler(async (req, res) => {
     if (!email || !password) {
         res.status(400)
         throw new Error("All fields are mandatory!")
+    }
+
+    const user = await User.findOne({ email })
+    //compare password with hashed password
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "5m" }
+        )
+        res.status(200).json({ accessToken })
+    } else {
+        res.status(401)
+        throw new Error("Email or Password is not valid")
     }
 })
 
